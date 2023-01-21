0bfc67b718547733daaab65f96684e5b4763aca8
Berit Carvalho <beritcarvalho@gmail.com>
2023-01-21 13:39:01 -0300

    - Adicionado DbContext

---
+++ CondominiumApi.Infrastructure.IoC/DependencyInjections/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CondominiumApi.Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumApi.Infrastructure.IoC.DependencyInjections
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region DataBaseConnection
+            services.AddDbContext<DataBaseContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            #endregion
+
+            return services;
+        }
+    }
+}
