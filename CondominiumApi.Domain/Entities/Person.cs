3facecb329d52b032290dcbdb3b7a17578d8bd30
Berit Carvalho <beritcarvalho@gmail.com>
2023-01-22 11:59:25 -0300

    - Adicionado Entidade de Pessoas

---
+++ CondominiumApi.Domain/Entities/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumApi.Domain.Entities
+{
+    public class Person : Entity
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Cpf { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Create_Date { get; set; }
+        public DateTime? Last_Update_Date { get; set; }
+    }
+}
