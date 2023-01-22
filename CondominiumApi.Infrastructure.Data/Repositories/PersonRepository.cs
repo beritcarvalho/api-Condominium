103c4d33f169d7cfd7512f745aed3f5354a1c69a
Berit Carvalho <beritcarvalho@gmail.com>
2023-01-22 12:29:33 -0300

    - Adicionado PersonRepository

---
+++ CondominiumApi.Infrastructure.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using CondominiumApi.Domain.Entities;
+using CondominiumApi.Infrastructure.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumApi.Infrastructure.Data.Repositories
+{
+    public class PersonRepository : BaseRepository<Person>
+    {
+        public PersonRepository(DataBaseContext context) : base(context)
+        {
+        }
+    }
+}
