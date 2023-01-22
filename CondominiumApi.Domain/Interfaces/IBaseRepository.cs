2f9c36c4ce1bb61af3b4d54410dd0e95e8c8a613
Berit Carvalho <beritcarvalho@gmail.com>
2023-01-22 12:41:59 -0300

    - Adicionado Interfaces dos Repositorios: --Base --Person

---
+++ CondominiumApi.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumApi.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> InsertAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+    }
+}
