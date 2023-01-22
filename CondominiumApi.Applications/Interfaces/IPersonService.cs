1ec85337800a653ffd67d49370597a7f8c3b6ff8
Berit Carvalho <beritcarvalho@gmail.com>
2023-01-22 12:31:22 -0300

    - Adicionado PersonService - Adicionado Interface de Person - Adicionado InputModel de Person - Adicionado ViewModel de Person

---
+++ CondominiumApi.Applications/Interfaces/IPersonService.cs
@@ -0,0 +1,19 @@
+﻿using CondominiumApi.Applications.Dtos.InputModels;
+using CondominiumApi.Applications.Dtos.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumApi.Applications.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<IList<PersonViewModel>> GetAll();
+        Task<PersonViewModel> GetById(int id);
+        Task<PersonViewModel> AddAccount(PersonInputModel input);
+        Task<PersonViewModel> UpdateAccount(PersonInputModel input);
+        Task<PersonViewModel> RemoveById(int id);
+    }
+}
---
+++ CondominiumApi.Applications/Services/PersonService.cs
@@ -0,0 +1,38 @@
+﻿using CondominiumApi.Applications.Dtos.InputModels;
+using CondominiumApi.Applications.Dtos.ViewModels;
+using CondominiumApi.Applications.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumApi.Applications.Services
+{
+    public class PersonService : IPersonService
+    {
+        public async Task<IList<PersonViewModel>> GetAll() 
+        {
+            throw new NotImplementedException();
+        }
+        public Task<PersonViewModel> AddAccount(PersonInputModel input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PersonViewModel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PersonViewModel> RemoveById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PersonViewModel> UpdateAccount(PersonInputModel input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
