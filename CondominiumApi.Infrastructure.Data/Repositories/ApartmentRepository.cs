765b9809884b11d9f60fd24c442d92ba276451ce
Berit Carvalho <beritcarvalho@gmail.com>
2023-01-26 22:19:45 -0300

    - Adicionado Repositorios do Apartamento e do Bloco

---
+++ CondominiumApi.Infrastructure.Data/Repositories/ApartmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using CondominiumApi.Domain.Entities;
+using CondominiumApi.Infrastructure.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumApi.Infrastructure.Data.Repositories
+{
+    public class ApartmentRepository : BaseRepository<Apartment>
+    {
+        public ApartmentRepository(DataBaseContext context) : base(context)
+        {
+        }
+    }
+}
