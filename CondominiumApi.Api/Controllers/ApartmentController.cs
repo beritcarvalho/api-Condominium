b8e0a94b5d5b763a590b533e6c70903916290af5
Berit Carvalho <beritcarvalho@gmail.com>
2023-01-27 07:37:47 -0300

    - Adicionado controller Apartments

---
+++ CondominiumApi.Api/Controllers/ApartmentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CondominiumApi.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/apartments")]
+    public class ApartmentController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Teste()
+        {
+            return Ok();
+        }
+    }
+}
